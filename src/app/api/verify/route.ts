59be02b62cac4a5ba8daa9a88ad8413497852b6e
Rob Knight <mail@robknight.org.uk>
2025-06-03 20:57:16 +0200

    Revert "Try different file path"
    This reverts commit 57f5ece685c4e0cc4cfba5f06c78388179123683.

--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -6,10 +6,7 @@ import path from "path";
 // @ts-ignore ffjavascript does not have types
 import { getCurveFromName } from "ffjavascript";
 
-const GPC_ARTIFACTS_PATH = path.join(
-  process.cwd(),
-  "node_modules/@pcd/proto-pod-gpc-artifacts"
-);
+const GPC_ARTIFACTS_PATH = path.join(process.cwd(), "public/artifacts");
 
 export async function POST(req: NextRequest) {
   const { serializedProofResult } = await req.json();
